1d84066831b72487814dda34c098d9f16bf9620d
Rodolfo Resines <rodolfo.sresines@gmail.com>
2018-05-24 18:27:28 -0500

    created the html framework and added some css stylings, started working on javascript

---
+++ assets/app.js
@@ -0,0 +1,86 @@
+$(document).ready(function() {
+
+    // Variable declaration
+    var correctAns = 0;
+    var incorrectAns = 0;
+
+    //Array of questions and answers
+    // game questions based on here: https://www.buzzfeed.com/emmacooke24/can-you-score-100-in-this-incredibly-hard-rick-and-morty
+    var questionsAns = [ 
+        { 
+        q:"What does Rick use to travel between dimensions and universes?", 
+        a:[
+        "Space Laser",
+        "Portal Gun",
+        "Tardis",
+        "Universe Key"], 
+        correct:"b"
+        },
+        { 
+        q:"Who is Morty based on?", 
+        a:[
+        "Frankenstein",
+        "Marty from Back to The Future",
+        "Hugo Strange",
+        "Adam Webber from Blast from the Past"
+            ], 
+        correct:"b" 
+        },
+        { 
+        q:"Who are Rick's two best friends?", 
+        a:[
+        "Birdperson and Squanchy",
+        "Eagleperson and Scrunchy",
+        "Beakperson and Squinchy",
+        "Hawkperson and Sqelchy"
+            ], 
+        correct:"a"
+        },
+        { 
+        q:"What are the Ball Fondlers?", 
+        a:[
+        "Rick and Morty's arch nemisis" ,
+        "Rick and Morty's most hated TV show",
+        "Rick and Morty's favourite TV show",
+        "A crime fighting team that fights crime"
+            ], 
+        correct:"c"  
+        },
+        { 
+        q:"What is the name of the park that Rick builds inside an Australian homeless man?", 
+        a:[
+        "Anatomy Park" ,
+        "Anatomical Fair",
+        "Anatomy Works",
+        "Anatomical World"
+                ], 
+        correct:"a"  
+            },
+        { 
+        q:"What is Scary Terry's catchphrase?", 
+        a:[
+        "Here's looking at you" ,
+        "Kablam",
+        "Bitch",
+        "Dickhead"
+                        ], 
+        correct:"c"  
+                    }, 
+        { 
+        q:"What is Scary Terry's catchphrase?", 
+        a:[
+        "Here's looking at you" ,
+        "Kablam",
+        "Bitch",
+        "Dickhead"
+                                        ], 
+        correct:"c"  
+                                    },    
+    ]
+
+    // function that on click start button changes the content to the first question of the game
+    // use .replacewith() http://api.jquery.com/replacewith/
+
+
+
+});
